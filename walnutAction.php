b59e87f8568209482fa896b26c1a4de6fd44bcdc
Robert Dunn <rcdunnii@gmail.com>
2013-05-22 15:01:18 -0500

    Finished backup download functions...

--- walnutAction.php
+++ walnutAction.php
@@ -122,12 +122,12 @@
         //if the db directory doesn't exist yet, create it
         if (!is_dir($dir))
             mkdir($dir);  
-        //create the file name for the backup (if you want to run the update more frequently than once a day, add more specificity to the date
+        //create the file name for the backup 
         $backupfile = $dir.'/'.$database.'_'.date("m-d-Y_H-i-s").'.sql';   
         //make the system call to mysqldump
-        exec("mysqldump --user=$user --password=$password --host=$server $database > $backupfile", $output, $result);           
-        if ($result == 0) {
-            header("Content-Description: File Transfer"); 
+        exec("mysqldump --user=$user --password=$password --host=$server $database > $backupfile", $output, $result);
+        if ($result == 0) {           
+ /*           header("Content-Description: File Transfer"); 
             header("Content-Type: application/octet-stream"); 
             header("Content-Disposition: attachment; filename=\"$backupfile\"");
             header('Expires: 0');
@@ -141,8 +141,12 @@
             var_dump($output);
             echo "$backupfile unable to be created - error # $result";
         }
-        return;        
-    }
+*/
+            echo ("<a href=\"$backupfile\" download>Backup OK! Click to Download</a>"); 
+        } else {
+            echo ("Backup failed with error # " . $result);
+        }
+}        
 	
 	if (($_REQUEST['value']) != "") {
 	   $whichDashBoardOpt = $_REQUEST['value'];
